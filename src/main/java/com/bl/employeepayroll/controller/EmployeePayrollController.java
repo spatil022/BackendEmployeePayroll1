3e521a7ffebcd123c4257871825a2417ddd1839b
spatil022 <shamalpatil1998@gmail.com>
2021-05-13 19:44:51 +0530

    [Shamal]Added: Created Rest Controller to demonstrate the various HTTP Methods

--- src/main/java/com/bl/employeepayroll/controller/EmployeePayrollController.java
+++ src/main/java/com/bl/employeepayroll/controller/EmployeePayrollController.java
@@ -1,15 +1,42 @@
 package com.bl.employeepayroll.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.bl.employeepayroll.dto.EmployeePayrollDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/hello")
+@RequestMapping("/employeepayrollservice")
 public class EmployeePayrollController {
 
-    @GetMapping("/gethello")
-    public String getHello() {
-        return "Hello World";
+    //curl localhost:8080/employeepayrollservice/get -w "\n"
+    @GetMapping("/get")
+    public ResponseEntity<String> getEmployeePayrollData() {
+        return new ResponseEntity<String>("Get Call Success", HttpStatus.OK);
     }
+
+    //curl localhost:8080/employeepayrollservice/get/1 -w "\n"
+    @GetMapping("/get/{empId}")
+    public ResponseEntity<String> getEmployeePayrollData(@PathVariable("empId") int empId) {
+        return new ResponseEntity<String>("Get Call Success for id:" + empId, HttpStatus.OK);
+    }
+
+    // curl -X POST -H "Content-Type: application/json" -d '{"name": "Lisa","salary":2000}' "http://localhost:8080/employeepayrollservice/create" -w "\n"
+    @PostMapping("/create")
+    public ResponseEntity<String> addEmployeePayrollData(@RequestBody EmployeePayrollDTO employeePayrollDto) {
+        return new ResponseEntity<String>("Created Employee payroll data for:" + employeePayrollDto, HttpStatus.OK);
+    }
+
+    //curl -X PUT -H "Content-Type: application/json" -d '{"name": "Lisa","salary": 2000}' "http://localhost:8080/employeepayrollservice/update" -w "\n"
+    @PutMapping("/update")
+    public ResponseEntity<String> updateEmployeePayrollData(@RequestBody EmployeePayrollDTO employeePayrollDto) {
+        return new ResponseEntity<String>("Updated Employee payroll Data for: " + employeePayrollDto, HttpStatus.OK);
+    }
+
+    // curl -X DELETE -H "Content-Type: application/json" localhost:8080/employeepayrollservice/delete/1 -w "\n"
+    @DeleteMapping("/delete/{empId}")
+    public ResponseEntity<String> deleteEmployeePayrollData(@PathVariable("empId") int empId) {
+        return new ResponseEntity<String>("Delete Call Success for id: " + empId, HttpStatus.OK);
+    }
+
 }
---
+++ src/main/java/com/bl/employeepayroll/dto/EmployeePayrollDTO.java
@@ -0,0 +1,16 @@
+package com.bl.employeepayroll.dto;
+
+public class EmployeePayrollDTO {
+    public String name;
+    public long salary;
+
+    public EmployeePayrollDTO(String name, long salary){
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String toString() {
+        return "EmployeePayrollDTO [name=" + name + ", salary=" + salary + "]";
+    }
+
+}
